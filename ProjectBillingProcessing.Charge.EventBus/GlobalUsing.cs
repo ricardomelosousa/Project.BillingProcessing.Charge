65ac316105aab4dbfdced5046590582acf64c541
ricardomelosousa <ricardo_melo_sousa@hotmail.com>
2022-08-10 01:21:21 -0300

    Delete ProjectBillingProcessing.Charge.EventBus directory
    .

--- ProjectBillingProcessing.Charge.EventBus/GlobalUsing.cs
+++
@@ -1,6 +0,0 @@
-﻿global using Microsoft.Extensions.Logging;
-global using Polly;
-global using Polly.Retry;
-global using RabbitMQ.Client;
-global using RabbitMQ.Client.Exceptions;
-global using System.Net.Sockets;
